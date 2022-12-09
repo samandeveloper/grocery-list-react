a2b2f41a020bebd31363410ecb1d5b9f4d38067f
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-09 11:14:53 -0700

    Updates

---
+++ precache-manifest.54b0edf93d378dbd7de1159b78402ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4667101b471e62cfc5ec05d272977cb4",
+    "url": "/grocery-list-react/index.html"
+  },
+  {
+    "revision": "9ec7273c05dc194efc82",
+    "url": "/grocery-list-react/static/css/main.31fc9bf1.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-list-react/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-list-react/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec7273c05dc194efc82",
+    "url": "/grocery-list-react/static/js/main.d9f86c2d.chunk.js"
+  },
+  {
+    "revision": "34cd2fa18211896e48e4",
+    "url": "/grocery-list-react/static/js/runtime-main.d8f38506.js"
+  }
+]);
